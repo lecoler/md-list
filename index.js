ee8f115b92c51383ecc0814758eb8b7c5c065a57
lecoler <wu870027945@163.com>
2019-10-31 15:17:23 +0800

    新增右键回到顶部

--- index.js
+++ index.js
@@ -42,9 +42,10 @@
         $menu.className = 'hidden';
         $button.setAttribute('class', 'le-md-btn');
         $button.innerHTML = `目录`;
-        $button.title = '右键返回顶部(to Top)';
+        $button.title = '右键返回顶部(RM to Top)';
         // 添加右键点击事件
         $button.oncontextmenu = e => {
+            // 回到顶部
             scrollTo(0, 0);
             return false;
         };
